6a6e59af39c8a70090a7381df65f1c92c7bf3f22
maruring <maruring3745@gmail.com>
2025-01-09 14:57:03 +0900

    リファクタリング

--- AWS/CDK/basic/lib/resource/subnet.ts
+++ AWS/CDK/basic/lib/resource/subnet.ts
@@ -3,6 +3,14 @@ import { CfnVPC, CfnSubnet } from "aws-cdk-lib/aws-ec2";
 // 自作モジュール
 import { Resource } from "./abstract/resources";
 
+interface ResourceInfo {
+    readonly id: string;
+    readonly cidrBlock: string;
+    readonly availabilityZone: string;
+    readonly resourceName: string;
+    readonly assign: (subnet: CfnSubnet) => void;
+}
+
 export class Subnet extends Resource {
     public publicSubnet1a: CfnSubnet;
     public publicSubnet1c: CfnSubnet;
@@ -10,6 +18,36 @@ export class Subnet extends Resource {
     public privateSubnet1c: CfnSubnet;
 
     private readonly vpc: CfnVPC;
+    private readonly resourcesInfo: ResourceInfo[] = [
+        {
+            id: 'publicSubnet1a',
+            cidrBlock: '10.0.11.0/24',
+            availabilityZone: 'ap-northeast-1a',
+            resourceName: 'public-subnet-public-1a',
+            assign: subnet => this.publicSubnet1a = subnet
+        },
+        {
+            id: 'publicSubnet1c',
+            cidrBlock: '10.0.12.0/24',
+            availabilityZone: 'ap-northeast-1c',
+            resourceName: 'public-subnet-public-1c',
+            assign: subnet => this.publicSubnet1c = subnet
+        },
+        {
+            id: 'privateSubnet1a',
+            cidrBlock: '10.0.21.0/24',
+            availabilityZone: 'ap-northeast-1a',
+            resourceName: 'private-subnet-public-1a',
+            assign: subnet => this.privateSubnet1a = subnet
+        },
+        {
+            id: 'privateSubnet1c',
+            cidrBlock: '10.0.22.0/24',
+            availabilityZone: 'ap-northeast-1c',
+            resourceName: 'private-subnet-public-1c',
+            assign: subnet => this.privateSubnet1c = subnet
+        }
+    ]
 
     constructor (vpc: CfnVPC) {
         super();
@@ -17,35 +55,20 @@ export class Subnet extends Resource {
     };
 
     public createResource(scope: Construct) {
-        const applicationName = scope.node.tryGetContext('applicationName');
-        const env = scope.node.tryGetContext('env');
+        this.resourcesInfo.forEach((resourceInfo) => {
+            const subnet = this.createSubnet(scope, resourceInfo);
+            resourceInfo.assign(subnet);
+        })
+    }
 
-        this.publicSubnet1a = new CfnSubnet(scope, 'publicSubnet1a', {
+    private createSubnet(scope: Construct, resourceInfo: ResourceInfo): CfnSubnet {
+        const subnet = new CfnSubnet(scope, resourceInfo.id, {
             vpcId: this.vpc.ref,
-            cidrBlock: '10.0.11.0/24',
-            availabilityZone: 'ap-northeast-1a',
-            tags: [{key: 'Name', value: this.createResourceName(scope, 'public-subnet-public-1a')}]
+            cidrBlock: resourceInfo.cidrBlock,
+            availabilityZone: resourceInfo.availabilityZone,
+            tags: [{key: 'Name', value: this.createResourceName(scope, resourceInfo.resourceName)}]
         });
 
-        this.publicSubnet1c = new CfnSubnet(scope, 'publicSubnet1c', {
-            vpcId: this.vpc.ref,
-            cidrBlock: '10.0.12.0/24',
-            availabilityZone: 'ap-northeast-1c',
-            tags: [{key: 'Name', value: this.createResourceName(scope, 'public-subnet-public-1c')}]
-        });
-
-        this.privateSubnet1a = new CfnSubnet(scope, 'privateSubnet1a',{
-            vpcId: this.vpc.ref,
-            cidrBlock: '10.0.21.0/24',
-            availabilityZone: 'ap-northeast-1a',
-            tags: [{key: 'Name', value: this.createResourceName(scope, 'private-subnet-public-1a')}]
-        });
-
-        this.privateSubnet1c = new CfnSubnet(scope, 'privateSubnet1c', {
-            vpcId: this.vpc.ref,
-            cidrBlock: '10.0.22.0/24',
-            availabilityZone: 'ap-northeast-1c',
-            tags: [{key: 'Name', value: this.createResourceName(scope, 'private-subnet-public-1c')}]
-        });
+        return subnet;
     }
 };
