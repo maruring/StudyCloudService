f5f79007e28b74b0be466609eb86227b243e8204
maruring <maruring3745@gmail.com>
2025-01-13 14:12:12 +0900

    add dynamoDB

--- AWS/CDK/serverless/lib/serverless-stack.ts
+++ AWS/CDK/serverless/lib/serverless-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CfnBucket } from 'aws-cdk-lib/aws-s3';
+import { CfnTable } from 'aws-cdk-lib/aws-dynamodb';
 // 独自モジュール
 import { EnvProps } from '../bin/serverless';
 
@@ -9,16 +10,41 @@ export class ServerlessStack extends cdk.Stack {
     super(scope, id, props);
     // TODO: 以下に必要なリソースを立ち上げる
     this.createS3Bucket(envProps);
+    this.createDynamoDB(envProps);
   }
 
   private createS3Bucket(envProps: EnvProps) {
     const applicationName = envProps.applicationName.toLocaleLowerCase();
-    new CfnBucket(this, `${envProps.envUpperCase}-${envProps.applicationName}-S3`,{
+    new CfnBucket(this, `${envProps.envUpperCase}-${envProps.applicationName}-S3`, {
       bucketName: `${envProps.envLowerCase}-${applicationName}`,
     })
   };
 
-  private createDynamoDB(scope: Construct) {
-    return;
+  private createDynamoDB(envProps: EnvProps) {
+    const applicationName = envProps.applicationName.toLocaleLowerCase();
+    new CfnTable(this, `${envProps.envUpperCase}-${envProps.applicationName}-DynamoTable`, {
+      tableName: `${envProps.envLowerCase}-${applicationName}`,
+      attributeDefinitions: [
+        {
+          attributeName: 'userId',
+          attributeType: 'S'
+        },
+        {
+          attributeName: 'taskId',
+          attributeType: 'S'
+        }
+      ],
+      keySchema: [
+        {
+          attributeName: 'userId',
+          keyType: 'HASH'
+        },
+        {
+          attributeName: 'taskId',
+          keyType: 'RANGE'
+        }
+      ],
+      billingMode: 'PAY_PER_REQUEST'
+    })
   };
 }
